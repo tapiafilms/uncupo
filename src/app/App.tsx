572bd64f2bfd5e39bc61ed36165de75738c7976b
Pablo Tapia <53624699+tapiafilms@users.noreply.github.com>
2026-04-13 21:55:35 -0400

    fix: correct queryClient import path

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./lib/queryClient";
+import { queryClient } from "../lib/queryClient";
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes";
